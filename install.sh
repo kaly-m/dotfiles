fdc376cb67c86a01473e380afd3f2735bcae1c9d
Kelian MICHIEL <kelian.michiel@gmail.com>
2025-08-15 18:38:06 +0200

    fix path .dotfile

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 # X11 #
 #######
 rm -rf "$HOME/.config/X11"
-ln -s "$HOME/.dotfiles/X11/" "$HOME/.config"
+ln -s "$HOME/dotfiles/X11" "$HOME/.config"
